b6e1f5b4022b595205aa58491bdcbb0a99f4d5c1
Roman Yakovlev <royakovlev@cloud.ru>
2024-07-10 20:01:22 +0300

    add build metadata vars

--- cmd/shortener/main.go
+++ cmd/shortener/main.go
@@ -1,12 +1,31 @@
 package main
 
 import (
+	"fmt"
 	"log"
 
 	"github.com/romanyakovlev/go-yandex-url-shortener/internal/server"
 )
 
+var (
+	buildVersion string
+	buildDate    string
+	buildCommit  string
+)
+
+func valueOrNA(value string) string {
+	if value == "" {
+		return "N/A"
+	}
+	return value
+}
+
 func main() {
+	formattedString := fmt.Sprintf(
+		"Build version: %s\nBuild date: %s\nBuild commit: %s",
+		valueOrNA(buildVersion), valueOrNA(buildDate), valueOrNA(buildCommit),
+	)
+	fmt.Println(formattedString)
 	if err := server.Run(); err != nil {
 		log.Fatalf("An error occurred: %v", err)
 	}
